3de9267f7ce6dd357229acbdc9d58821272887dd
Ilia Spiridonov <ilya.sp.work@gmail.com>
2023-03-07 19:27:29 +0100

    extend interfaces

--- src/parser.rs
+++ src/parser.rs
@@ -2,38 +2,54 @@ use std::{collections::HashMap, env};
 
 pub enum OptionalArgKind {
     Flag,
-    Value,
+    RequiredValue,
+    OptionalValue,
 }
 
 pub struct OptionalArg {
-    kind: OptionalArgKind,
-    multiple: bool,
+    pub(crate) kind: OptionalArgKind,
+    pub(crate) multiple: bool,
+}
+
+pub enum ArgParsingMode {
+    Mixed,
+    OptionsFirst,
 }
 
 pub struct ArgParser {
+    pub(crate) mode: ArgParsingMode,
     pub(crate) aliases: HashMap<&'static str, &'static str>,
     pub(crate) options: HashMap<&'static str, OptionalArg>,
 }
 
 pub enum ParsedArg {
     Positional { value: String },
-    OptionalFlag { name: String, value: bool },
+    Flag { name: String, value: bool },
+    RequiredValue { name: String, value: String },
     OptionalValue { name: String, value: Option<String> },
 }
 
+pub enum ArgParserError {
+    InvalidOptionName { name: String },
+    UnknownOption { name: String },
+}
+
+type ArgParseResult = Result<Vec<ParsedArg>, ArgParserError>;
+
 impl ArgParser {
-    pub fn new() -> Self {
+    pub fn new(mode: ArgParsingMode) -> Self {
         Self {
+            mode,
             aliases: HashMap::new(),
             options: HashMap::new(),
         }
     }
 
-    pub fn parse(args: &[&str]) -> Vec<ParsedArg> {
-        vec![]
+    pub fn parse(args: &[&str]) -> ArgParseResult {
+        Ok(vec![])
     }
 
-    pub fn parse_args() -> Vec<ParsedArg> {
+    pub fn parse_args() -> ArgParseResult {
         let args = env::args().skip(1).collect::<Vec<_>>();
 
         Self::parse(&args.iter().map(|s| &s[..]).collect::<Vec<_>>())
